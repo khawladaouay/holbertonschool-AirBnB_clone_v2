557bf73ab6245476a7550d46ed8bbe546d5117c0
khawladaouay <daouaykhawla782@gmail.com>
2022-08-25 16:30:18 +0100

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web servers"""
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.91.126.79', '18.208.141.223']
+
+
+def do_deploy(archive_path):
+    if exists(archive_path):
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}{}".format(path, no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, no_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {0}{1}/web_static".format(path, no_ext))
+        run("rm -rf /data/web_static/current".format(path, no_ext))
+        run(" rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
+        return True
+
+    return False
