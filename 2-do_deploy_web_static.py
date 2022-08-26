dee39f9cb9e5b339822c01a544b55d84720bf691
khawladaouay <daouaykhawla782@gmail.com>
2022-08-26 15:56:40 +0100

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,18 +6,31 @@ env.hosts = ['54.91.126.79', '18.208.141.223']
 
 
 def do_deploy(archive_path):
-    """deploy func"""
+    """Prototype: def do_deploy(archive_path)"""
     if exists(archive_path):
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
+
+        file_name= archive_path.split("/")[-1]
+        remove = file_name.split(".")[0]
         path = "/data/web_static/releases/"
+
         put(archive_path, '/tmp/')
-        run("mkdir -p {}{}".format(path, no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, no_ext))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}{1}/web_static".format(path, no_ext))
-        run("rm -rf /data/web_static/current".format(path, no_ext))
-        run(" rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
+
+        run('mkdir -p {}{}/'.foremoveat(path, remove))
+
+        run('tar -xzf /tmp/{} -C {}{}/'.foremoveat(file_name, path, remove))
+
+        run('rm /tmp/{}'.format(file_name))
+
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, remove))
+
+        run('rm -rf {}{}/web_static'.format(path, remove))
+
+        run('rm -rf /data/web_static/current')
+
+        run('ln -s {}{}/ /data/web_static/current'.format(path, remove))
+
+        print("New version deployed!")
+
         return True
+
     return False
