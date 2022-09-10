996b85c40458ae175016014f5eee154057d10468
khawladaouay <daouaykhawla782@gmail.com>
2022-09-10 20:15:22 +0100

    task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_():
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_():
+    return "HBNB!"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text = text.replace('_', ' ')
+    return f"python {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
